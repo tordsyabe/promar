1bcd95e3fc201513d28fcdcf3d81eb56d23f0243
tordsyabe <46318298+tordsyabe@users.noreply.github.com>
2020-07-30 05:13:26 +0400

    fixed links href

--- main.js
+++ main.js
@@ -6,14 +6,3 @@ $(document).ready(function () {
   });
   $(".sidenav").sidenav();
 });
-
-$(function () {
-  $("a[href*=#]").on("click", function (e) {
-    e.preventDefault();
-    $("html, body").animate(
-      { scrollTop: $($(this).attr("href")).offset().top },
-      500,
-      "linear"
-    );
-  });
-});
